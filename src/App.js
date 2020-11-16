5ec4b2c22ddd2116247da10c90c26fffc0d1d394
kleekie <kyra.kwak@gmail.com>
2020-11-16 00:07:39 -0500

    completed page structure

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
 import './App.css';
 import axios from 'axios'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 const URL = 'https://api.spotify.com/v1/artists/1U0sIzpRtDkvu1hXXzxh60/related-artists'
-const token = 'Bearer BQBH_WTNAPCmKkIRLBONW7bqwdmIHzQjTlvJgfF650xKEnpbC3vvr-GZ1JyzbBMgUgg9Dkhd2Pd-DfakfHEsYf6TzMt9-QtlgwCrv6hvfEMDVeRPWENQpXR53ymtqlwdxP8FZMRoVJI'
+const token = 'Bearer BQDu3aAptmW9NbZucxQ9ghYuqm3-rCl_LFZRLUFhbwP5sWGJKOHStUQ1yhxC75kBTSmIS-VOtdGxNBEXRSYhcGXByJef2bpYZSe-egEgdw83q23uFhJPoxQEN9wyWJ3O-iegTSYa0ayjqkd6H7SIBhMf'
 
 function App() {
-  const data = Spodifyfunction()
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
-
-
-
-
-
-
-const Spodifyfunction = async function(URL){
-	const response = await axios(
-	{
-		method:'get',
-		url: URL,		
-		dataType: 'json',
-		headers:{
-			'Authorization':token,
-			'Content-Type':'json'			
+	const [data,setData]= useState([])
+	useEffect(async () => {
+		const response = await axios(
+				{
+					method:'get',
+					url: URL,		
+					dataType: 'json',
+					headers:{
+						'Authorization':token,
+						'Content-Type':'json'			
+					}
+				}
+			)
+			setData(response.data.artists)
+	  },[]);
+	  console.log(data,'test data')
+	  const finalData = data.reduce((accum,{name,genres,external_urls,followers,popularity,images})=> {
+	
+		if(!accum[name]){
+			accum[name]={genres,external_urls,followers,popularity,images}
+			return accum
 		}
-	}
-)
-const {data:{artists}}=response
-return artists
+		
+	  },{})
+
+console.log(finalData,'finaldata')
+const finalList = Object.keys(finalData).map((name,index)=>{
+	console.log(name,'name')
+	let {images}=finalData[name]
+	let {external_urls:{spotify}}=finalData[name]
+	return <li className="artist-list-item" key={index}>
+		<div className="artist-name"><a className="external-link" href={spotify}>{name}</a></div>
+		<div className="artist-portrait-container">
+		<img className="artist-portrait" src={images[2]['url']}/>
+		</div>
+	</li>
+})
+
+return (
+	<div className="App">
+		<ul className="artist-list">
+			{finalList}
+		</ul>
+	</div>
+);
 }
-
-
 
 
 export default App;
